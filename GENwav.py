4510dde17993fced43219c0b8654d8815c806201
feli-p <Felipe-0019@hotmail.com>
2023-11-03 15:06:24 -0600

    Changos

--- GENwav.py
+++ GENwav.py
@@ -1,238 +1,163 @@
 """
-GENwav replica el sonido de un osciloscopio usando algoritmos evolutivos.
+Algoritmo genético para replicar el sonido de osciloscopios específicos.
 
 Hecho por Losa lucines GPT.
 """
-#%%
 
-import numpy as np
-import scipy.io.wavfile as wav
-# from scipy import signal
-import matplotlib.pyplot as plt
+from numpy.random import randint
+from numpy.random import rand
+from libs.aluSynth import Synth
 
-SAMPLE_RATE = 44100 # Hz
-SAMPLE_LENGTH = 4 # Sec
-N = SAMPLE_LENGTH * SAMPLE_RATE
-time = np.linspace(0,SAMPLE_LENGTH,N) # vector de tiempo
+# objective function
+def objective(x):
+	return x[0]**2.0 + x[1]**2.0
 
-# Señales
-def sawtooth(x):
-    return (x + np.pi) / np.pi % 2 - 1
+# decode bitstring to numbers
+def decode(bounds, n_bits, bitstring):
+	decoded = list()
+	largest = 2**n_bits
+	for i in range(len(bounds)):
+		# extract the substring
+		start, end = i * n_bits, (i * n_bits)+n_bits
+		substring = bitstring[start:end]
+		# convert bitstring to a string of chars
+		chars = ''.join([str(s) for s in substring])
+		# convert string to integer
+		integer = int(chars, 2)
+		# scale integer to desired range
+		value = bounds[i][0] + (integer/largest) * (bounds[i][1] - bounds[i][0])
+		# store
+		decoded.append(value)
+	return decoded
 
-def square(x):
-    return 2*(2*np.floor(0.16*x) - np.floor(2*0.16*x)) + 1
+# tournament selection
+def selection(pop, scores, k=3):
+	# first random selection
+	selection_ix = randint(len(pop))
+	for ix in randint(0, len(pop), k-1):
+		# check if better (e.g. perform a tournament)
+		if scores[ix] < scores[selection_ix]:
+			selection_ix = ix
+	return pop[selection_ix]
 
-class Envelope:
-    # Envolvente lineal de la señal (fade in/out), modulación de amplitud (volumen) de la señal
-    def __init__(self):
-        self.name = ""
-        self.attack = 0.2 # sec
-        self.peak = 1 # % [0 - 1] 
-        self.decay = 0.5 # sec
-        self.sustain = 0.75 # % [0 - 1]
-        self.release = 1 # sec
-        self.sustain_length = 3 # sec
-    
-    def wave(self):
-        output = np.zeros((N,))
-        # obtener índices correspondientes
-        attack_indx = self.get_time_index(self.attack)
-        decay_indx = self.get_time_index(self.attack+self.decay)
-        sustain_indx = self.get_time_index(self.attack+self.decay+self.sustain_length)
-        release_indx = self.get_time_index(self.attack+self.decay+self.sustain_length+self.release)
-        
-        # Definir el envolvente
-        CA = self.peak / self.attack
-        CD = (self.sustain - self.peak) / self.decay
-        CR_1 = self.attack + self.decay + self.sustain_length
-        CR_2 = -self.sustain / self.release
-        
-        a = self.peak-self.sustain
-        l1 = -7/(self.decay)
-        l2 = -5/self.release
+# crossover two parents to create two children
+def crossover(p1, p2, r_cross):
+	# children are copies of parents by default
+	c1, c2 = p1.copy(), p2.copy()
+	# check for recombination
+	if rand() < r_cross:
+		# select crossover point that is not on the end of the string
+		pt = randint(1, len(p1)-2)
+		# perform crossover
+		c1 = p1[:pt] + p2[pt:]
+		c2 = p2[:pt] + p1[pt:]
+	return [c1, c2]
 
-        output[decay_indx+1:sustain_indx+1] = self.sustain
-        for n in range(N):
-            if (n >= 0 and n <= attack_indx):
-                output[n] = time[n] * CA
-            elif (n > attack_indx and n <= decay_indx):
-                output[n] = a*np.exp(l1*(time[n]-self.attack)) + self.sustain
-            elif (n > sustain_indx):
-                output[n] = self.sustain*np.exp(l2*(time[n] - CR_1))
-            else:
-                pass
+# mutation operator
+def mutation(bitstring, r_mut):
+	for i in range(len(bitstring)):
+		# check for a mutation
+		if rand() < r_mut:
+			# flip the bit
+			bitstring[i] = 1 - bitstring[i]
 
-        plt.plot(time, output)
-        plt.show()
-        
-        return output
-    
-    def get_time_index(self, mark):
-        n = 0
-        while (n < N and time[n] < mark):
-            n += 1
-        return n
+# genetic algorithm
+def genetic_algorithm(objective, bounds, n_bits, n_iter, n_pop, r_cross, r_mut):
+	# initial population of random bitstring
+	pop = [randint(0, 2, n_bits*len(bounds)).tolist() for _ in range(n_pop)]
+	# keep track of best solution
+	best, best_eval = 0, objective(decode(bounds, n_bits, pop[0]))
+	# enumerate generations
+	for gen in range(n_iter):
+		# decode population
+		decoded = [decode(bounds, n_bits, p) for p in pop]
+		# evaluate all candidates in the population
+		scores = [objective(d) for d in decoded]
+		# check for new best solution
+		for i in range(n_pop):
+			if scores[i] < best_eval:
+				best, best_eval = pop[i], scores[i]
+				print(">%d, new best f(%s) = %f" % (gen,  decoded[i], scores[i]))
+		# select parents
+		selected = [selection(pop, scores) for _ in range(n_pop)]
+		# create the next generation
+		children = list()
+		for i in range(0, n_pop, 2):
+			# get selected parents in pairs
+			p1, p2 = selected[i], selected[i+1]
+			# crossover and mutation
+			for c in crossover(p1, p2, r_cross):
+				# mutation
+				mutation(c, r_mut)
+				# store for next generation
+				children.append(c)
+		# replace population
+		pop = children
+	return [best, best_eval]
 
-class LFO:
-    # "Low Frequency Oscilator", modulación de frecuencia de la señal
-    def __init__(self):
-        self.name = ""
-        self.frequency = 0 # Hz
-        self.ammount = 0 # (0 - 1) 
-        self.wave_form = "sine" # String (sin, sawtooth, square)
-    
-    def wave(self):
-        if (self.wave_form == "sine"):
-            waveform = np.sin
-        elif (self.wave_form == "sawtooth"):
-            waveform = sawtooth
-        elif (self.wave_form == "square"):
-            waveform = square
-        else:
-            return -1
-        
-        output = np.zeros((N,))
-        
-        w = 2*np.pi*self.frequency
-        for n in range(N):
-            output[n] = waveform(w*time[n])
-        
-        amplitude = 10 ** (0 / 2)
-        # output *= self.ammount
-        output *= amplitude
-
-        return output
-
-class Filter:
-    # Filter
-    def __init__(self):
-        self.name = ""
-        self.cutoff_freq = 150 # Hz
-        self.type = "lowpass" # String (lowpass, highpass)
-
-class OSC:
-    # Oscilador, señal principal o que modula en frecuencia a otra señal
-    def __init__(self):
-        self.name = ""
-        self.frequency = 440 # Hz
-        self.gain = -3 # dB
-        self.wave_form = "sine" # String (sin, sawtooth, square)
-        self.beta = 5 # Modulating coefficient
-        self.freq_modulator = np.zeros((N,))  # OSC
-        self.envelope = Envelope() # Envelope
-        self.LFO = LFO() # LFO
-    
-    def wave(self):
-        if (self.wave_form == "sine"):
-            waveform = np.sin
-        elif (self.wave_form == "sawtooth"):
-            waveform = sawtooth
-        elif (self.wave_form == "square"):
-            waveform = square
-        else:
-            return -1
-        
-        output = np.zeros((N,))
-        LFO_signal = self.LFO.wave()
-        
-        w = 2*np.pi*self.frequency
-        for n in range(N):
-            output[n] = waveform(w*(time[n]) + self.beta*self.freq_modulator[n] + self.LFO.ammount*LFO_signal[n])
-        
-        amplitude = 10 ** (self.gain / 2)
-        output *= amplitude
-        
-        # Envelope
-        output *= self.envelope.wave()
-        
-        # Filter
-        
-        return output
-    
-class Synth:
-    def __init__(self):
-        self.name = ""
-        self.osc_A = OSC()
-        self.osc_A.name = "OSC A"
-        self.osc_B = OSC()
-        self.osc_B.name = "OSC B"
-        self.filter = Filter() # Filter
-        self.LFO = LFO() # LFO
-        self.algorithm = 1 # Int ([1] B -> A, [2] A -> B, [3] A + B)
-    
-    def wave(self):
-        # crear un archivo .wav con la señal final
-        self.osc_A.LFO = self.LFO
-        self.osc_B.LFO = self.LFO
-        output = np.zeros((SAMPLE_LENGTH * SAMPLE_RATE,))
-        if (self.algorithm == 1):
-            # B modula A
-            self.osc_A.freq_modulator = self.osc_B.wave()
-            output = self.osc_A.wave()
-        elif (self.algorithm == 2):
-            # A modula B
-            self.osc_B.freq_modulator = self.osc_A.wave()
-            output = self.osc_B.wave()
-        else:
-            output = self.osc_A.wave() + self.osc_B.wave()
-        return output
-        
-    def write_wav(self, file_name):
-        output = self.wave()
-        file_name += ".wav"
-        wav.write(file_name, SAMPLE_RATE, output.astype(np.float32))
-    
-def main():
-    
-    # Instancia de un sintetizador
-    synth1 = Synth()
-    synth1.name = "Synth 1"
-    
-    # PARÁMETROS
-    
-    # Algoritmo para la modulación o adición ([1] A -> B , [2] B -> A , [3] A + B)
-    synth1.algorithm = 1 
-    # Parámetros del LFO
-    synth1.LFO.frequency = 1 # Hz
-    synth1.LFO.wave_form = "sine" # Forma de la señal
-    synth1.LFO.ammount = 0.0 # % [0 - 1] indica que tanto modula el LFO (0 lo desactiva)
-    # Parámetros del filtro
-    # TBD
-    # Parámetros del Oscilador A
-    synth1.osc_A.frequency = 1 # Hz
-    synth1.osc_A.gain = 0 # dB
-    synth1.osc_A.wave_form = "square" # Forma de la señal
-    synth1.osc_A.beta = 0 # Constante para la modulación (0 desactiva la modulación)
-    # Parámetros de la envolvente del Oscilador A
-    synth1.osc_A.envelope.attack = 0.01 # sec 
-    synth1.osc_A.envelope.peak = 0.5 # %
-    synth1.osc_A.envelope.decay = 2.0 # sec
-    synth1.osc_A.envelope.sustain = 0.25 # %
-    synth1.osc_A.envelope.sustain_length = 1.5 # sec
-    synth1.osc_A.envelope.release = 0.4 # sec
-    # Parámetros del Oscilador B
-    synth1.osc_B.frequency = 110 # Hz
-    synth1.osc_B.gain = -0.2 # dB
-    synth1.osc_B.wave_form = "square" # Forma de la señal
-    synth1.osc_B.beta = 0 # Constante para la modulación
-    # Parámetros de la envolvente del Oscilador B
-    synth1.osc_B.envelope.attack = 0.001 # sec 
-    synth1.osc_B.envelope.peak = 1.0 # %
-    synth1.osc_B.envelope.decay = 0.8 # sec
-    synth1.osc_B.envelope.sustain = 0.25 # %
-    synth1.osc_B.envelope.sustain_length = 1.5 # sec
-    synth1.osc_B.envelope.release = 0.4 # sec
-    
-    # Escribir archivo .wav
-    #synth1.write_wav("prueba")
-    
-    # Gráfica
-    #plt.plot(time, synth1.wave())
-    #plt.show()
-    synth1.osc_A.envelope.wave()
-    
-    
-    
+# define range for input
+bounds = [[-5.0, 5.0], [-5.0, 5.0]]
+# define the total iterations
+n_iter = 100
+# bits per variable
+n_bits = 16
+# define the population size
+n_pop = 100
+# crossover rate
+r_cross = 0.9
+# mutation rate
+r_mut = 1.0 / (float(n_bits) * len(bounds))
+# perform the genetic algorithm search
+best, score = genetic_algorithm(objective, bounds, n_bits, n_iter, n_pop, r_cross, r_mut)
+print('Done!')
+decoded = decode(bounds, n_bits, best)
+print('f(%s) = %f' % (decoded, score))
 
 if __name__ == '__main__':
-    main()
+	# Instancia de un sintetizador
+	synth1 = Synth()
+	synth1.name = "Synth 1"
+
+	# PARÁMETROS
+
+	# Algoritmo para la modulación o adición ([1] A -> B , [2] B -> A , [3] A + B)
+	synth1.algorithm = 1 
+	# Parámetros del LFO
+	synth1.LFO.frequency = 1 # Hz
+	synth1.LFO.wave_form = "sine" # Forma de la señal
+	synth1.LFO.ammount = 0.0 # % [0 - 1] indica que tanto modula el LFO (0 lo desactiva)
+	# Parámetros del filtro
+	# TBD
+	# Parámetros del Oscilador A
+	synth1.osc_A.frequency = 1 # Hz
+	synth1.osc_A.gain = 0 # dB
+	synth1.osc_A.wave_form = "square" # Forma de la señal
+	synth1.osc_A.beta = 0 # Constante para la modulación (0 desactiva la modulación)
+	# Parámetros de la envolvente del Oscilador A
+	synth1.osc_A.envelope.attack = 0.01 # sec 
+	synth1.osc_A.envelope.peak = 0.5 # %
+	synth1.osc_A.envelope.decay = 2.0 # sec
+	synth1.osc_A.envelope.sustain = 0.25 # %
+	synth1.osc_A.envelope.sustain_length = 1.5 # sec
+	synth1.osc_A.envelope.release = 0.4 # sec
+	# Parámetros del Oscilador B
+	synth1.osc_B.frequency = 110 # Hz
+	synth1.osc_B.gain = -0.2 # dB
+	synth1.osc_B.wave_form = "square" # Forma de la señal
+	synth1.osc_B.beta = 0 # Constante para la modulación
+	# Parámetros de la envolvente del Oscilador B
+	synth1.osc_B.envelope.attack = 0.001 # sec 
+	synth1.osc_B.envelope.peak = 1.0 # %
+	synth1.osc_B.envelope.decay = 0.8 # sec
+	synth1.osc_B.envelope.sustain = 0.25 # %
+	synth1.osc_B.envelope.sustain_length = 1.5 # sec
+	synth1.osc_B.envelope.release = 0.4 # sec
+
+	# Escribir archivo .wav
+	#synth1.write_wav("prueba")
+
+	# Gráfica
+	#plt.plot(time, synth1.wave())
+	#plt.show()
+	print('Hola')
+	synth1.osc_A.envelope.wave()
